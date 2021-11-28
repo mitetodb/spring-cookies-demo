85d7c41d3059550909af080da4f659a7a65e6f25
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-11-28 14:14:33 +0100

    project init

---
+++ src/test/java/bg/softuni/cookiesdemo/CookiesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.cookiesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
